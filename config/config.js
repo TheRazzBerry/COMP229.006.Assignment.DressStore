f96ebed132b91b9a5c5866286a3b653a117fc7ef
TheRazzBerry <razeen.hrahman@gmail.com>
2023-10-14 14:37:37 -0400

    Setup Environment Variables
    installed dotenv and added a .env file with MonogDB URI

--- config/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    "DBAUTH": "mongodb+srv://razzberry:comp229pass@comp229cluster.hkh7vxl.mongodb.net/DressStore?retryWrites=true&w=majority"
-}
--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
-// Hide Credentials
-let config = require('./config');
+// Environment Variables
+require('dotenv').config();
+const DB_URI = process.env.DB_URI;
 
 // Database Setup
 const mongoose = require('mongoose');
 
 module.exports = function() {
     //Get MongoDB URL
-    mongoose.connect(config.DBAUTH);
+    mongoose.connect(DB_URI);
     // Test Connection
     let mongodb = mongoose.connection;
     mongodb.on('error', console.error.bind(console, 'Connection Error: '));
