fc9350deaba802c3dc6fef0c9982a41eae685f23
TheRazzBerry <razeen.hrahman@gmail.com>
2023-10-14 07:55:12 -0400

    Connect to MongoDB
    Connected to MongoDB database and modified server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Define dependencies
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Define project constants
 const app = express();
@@ -8,6 +9,15 @@ const port = 3000;
 // Define routes
 app.get('/', (req, res) => {res.send('Hello World')});
 
-app.listen(port, () => {
-    console.log('Application is listening on PORT: ' + port);
+// Connect to MongoDB
+const mongoDB_url = "mongodb+srv://razzberry:comp229pass@comp229cluster.hkh7vxl.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoDB_url).then(() => {
+    console.log('Connected to MongoDB');
+
+    // Start the application and listen for events
+    app.listen(port, () => {
+        console.log('Application is listening on PORT: ' + port);
+    });
+}).catch((error) => {
+    console.log(error);
 });
